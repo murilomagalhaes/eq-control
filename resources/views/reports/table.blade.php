26816e36da63da8c65a82c623a06165d81d1cb5f
Murilo <murilomagalhaes@outlook.com>
2021-02-18 13:10:26 -0300

    Versão 1.0

--- resources/views/reports/table.blade.php
+++ resources/views/reports/table.blade.php
@@ -1,38 +1,45 @@
-<table class="table" style=" border: 1px solid black">
+<table>
     <thead>
-        <tr style="color: yellowgreen;">
-            <th scope="col" style="background-color: yellowgreen;">ID</th>
-            <th scope="col" style="background-color: yellowgreen;">Cliente</th>
-            <th scope="col" style="background-color: yellowgreen;">Entregue Por</th>
-            <th scope="col" style="background-color: yellowgreen;">Entrada</th>
-            <th scope="col" style="background-color: yellowgreen;">Previsao</th>
-            <th scope="col" style="background-color: yellowgreen;">Entrega</th>
+         <tr>
+            <th scope="col" style="background-color: lightgray;">ID</th>
+            <th scope="col" style="background-color: lightgray;">Cliente</th>
+            <th scope="col" style="background-color: lightgray;">Entregue Por</th>
+            <th scope="col" style="background-color: lightgray;">Entrada</th>
+            <th scope="col" style="background-color: lightgray;">Previsao</th>
+            <th scope="col" style="background-color: lightgray;">Entrega</th>
+        </tr>
+        <tr>
+            <th colspan="6"></th>
         </tr>
     </thead>
     <tbody>
-    <tr><td colspan="6"></td></tr>
         @foreach($registries as $i => $registry)
         <tr>
-            <th scope="row">{{$i + 1}}</th>
-            <td>{{$registry->customer->nome}}</td>
-            <td>{{$registry->nome}}</td>
-            <td>{{$registry->getFormatedDateTime('dt_entrada')}}</td>
-            <td>{{$registry->getFormatedDateTime('dt_previsao')}}</td>
-            <td>{{$registry->getFormatedDateTime('dt_entrega')}}</td>
+            <th scope="row" style="border-top: 1px solid #000000;">{{$registry->id}}</th>
+            <td style="border-top: 1px solid #000000;">{{$registry->customer->nome}}</td>
+            <td style="border-top: 1px solid #000000;">{{$registry->nome}}</td>
+            <td style="border-top: 1px solid #000000;">{{$registry->getFormatedDateTime('dt_entrada')}}</td>
+            <td style="border-top: 1px solid #000000;">{{$registry->getFormatedDateTime('dt_previsao')}}</td>
+            <td style="border-top: 1px solid #000000;">{{$registry->getFormatedDateTime('dt_entrega')}}</td>
         </tr>
 
         <tr></tr>
+        
         <tr>
             <td>
                 <table>
                     <tr>
-                        <th colspan="6" style="font-weight: bold; ">Equipamentos:</th>
+                        <th colspan="6" style="font-weight: bold; border-top: 1px solid #000000;">Equipamentos:</th>
                     </tr>
 
                     <tr>
                         @foreach($registry->equipments as $equipment)
                         <td>{{$equipment->descricao}}</td>
                         @endforeach
+                    </tr>
+
+                    <tr>
+                        <td colspan="6" style="border-top: 1px solid #000000;"></td>
                     </tr>
 
                 </table>
